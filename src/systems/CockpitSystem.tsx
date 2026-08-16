e58c0f3131aa2235d8793bf7976fee60cf742e5a
Márcio <bandolfinance@gmail.com>
2026-08-16 18:10:59 -0300

    Correção completa do CockpitSystem.tsx

--- src/systems/CockpitSystem.tsx
+++ src/systems/CockpitSystem.tsx
@@ -1,104 +1,45 @@
 import React from 'react';
-import PokerEventCore from './PokerEventCore';
-import PokerRoundFlow from './PokerRoundFlow';
-import HologramFXCore from '../components/cockpit/HologramFXCore';
-import SoundFXCore from '../components/cockpit/SoundFXCore';
-import LightFXCore from '../components/cockpit/LightFXCore';
-import HologramHUD from '../components/cockpit/HologramHUD';
-import NeonParticles from '../components/cockpit/NeonParticles';
-import DealerIA from '../components/dealer/DealerIA';
-import DealerIAReactive from '../components/dealer/DealerIAReactive';
-import FuturisticTable from '../components/table/FuturisticTable';
-import SoundSystem from './SoundSystem';
-import NeonSidePanels from '../components/cockpit/NeonSidePanels';
+
 import HUDEnergyBar from '../components/hud/HUDEnergyBar';
-import EdgeLights from '../components/cockpit/EdgeLights';
-import GlowGrid from '../components/cockpit/GlowGrid';
-import PulseCore from '../components/cockpit/PulseCore';
-import HologramRing from '../components/cockpit/HologramRing';
-import HologramScanline from '../components/cockpit/HologramScanline';
-import CornerGlow from '../components/cockpit/CornerGlow';
-import QuantumSparks from '../components/cockpit/QuantumSparks';
-import AmbientNebula from '../components/cockpit/AmbientNebula';
-import StarfieldWarp from '../components/cockpit/StarfieldWarp';
-import HologramRadar from '../components/cockpit/HologramRadar';
-import HologramCompass from '../components/cockpit/HologramCompass';
-import StatusPanel from '../components/cockpit/StatusPanel';
-import CockpitCommandPanel from '../components/cockpit/CockpitCommandPanel';
-import AdvancedIndicators from '../components/cockpit/AdvancedIndicators';
-import HologramSoundFX from '../components/cockpit/HologramSoundFX';
-import CockpitInteractionCore from '../components/cockpit/CockpitInteractionCore';
+import HUDPulse from '../components/hud/HUDPulse';
+import HUDIndicators from '../components/hud/HUDIndicators';
+import HUDRadar from '../components/hud/HUDRadar';
+import HUDCompass from '../components/hud/HUDCompass';
+import HUDRoundFlow from '../components/hud/HUDRoundFlow';
+import HUDActionFlow from '../components/hud/HUDActionFlow';
+import HUDIAState from '../components/hud/HUDIAState';
+import HUDProfilePanel from '../components/hud/HUDProfilePanel';
+import HUDMatrixGrid from '../components/hud/HUDMatrixGrid';
+import HUDThermalMap from '../components/hud/HUDThermalMap';
+import HUDOverdrivePulse from '../components/hud/HUDOverdrivePulse';
+import HUDNeuralScan from '../components/hud/HUDNeuralScan';
+import HUDSpectrumWave from '../components/hud/HUDSpectrumWave';
+import HUDQuantumField from '../components/hud/HUDQuantumField';
+import HUDCryoFlux from '../components/hud/HUDCryoFlux';
+import HUDVortexField from '../components/hud/HUDVortexField';
 
 const CockpitSystem: React.FC = () => {
   return (
-    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-      <PokerEventCore />
-      <PokerRoundFlow />
-      <HologramFXCore />
-      <SoundFXCore />
-      <LightFXCore />
-      <SoundSystem />
-      <HologramSoundFX />
-      <CockpitInteractionCore />
-      <AmbientNebula />
-      <StarfieldWarp />
-      <GlowGrid />
-      <PulseCore />
-      <HologramRing />
-      <HologramScanline />
-      <CornerGlow />
-      <QuantumSparks />
-      <NeonParticles />
-      <NeonSidePanels />
+    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
       <HUDEnergyBar />
-      <EdgeLights />
-      <HologramRadar />
-      <HologramCompass />
-      <StatusPanel />
-      <AdvancedIndicators />
-      <CockpitCommandPanel />
-      <HologramHUD />
-      <FuturisticTable />
-      <DealerIA />
-      <DealerIAReactive />
+      <HUDPulse />
+      <HUDIndicators />
+      <HUDRadar />
+      <HUDCompass />
+      <HUDRoundFlow />
+      <HUDActionFlow />
+      <HUDIAState />
+      <HUDProfilePanel />
+      <HUDMatrixGrid />
+      <HUDThermalMap />
+      <HUDOverdrivePulse />
+      <HUDNeuralScan />
+      <HUDSpectrumWave />
+      <HUDQuantumField />
+      <HUDCryoFlux />
+      <HUDVortexField />
     </div>
   );
 };
 
 export default CockpitSystem;
-import HUDCore from '../components/hud/HUDCore';
-<HUDCore />
-import HUDPulse from '../components/hud/HUDPulse';
-<HUDPulse />
-import HUDIndicators from '../components/hud/HUDIndicators';
-<HUDIndicators />
-import HUDRadar from '../components/hud/HUDRadar';
-<HUDRadar />
-import HUDCompass from '../components/hud/HUDCompass';
-<HUDCompass />
-import HUDRoundFlow from '../components/hud/HUDRoundFlow';
-<HUDRoundFlow />
-import HUDActionFlow from '../components/hud/HUDActionFlow';
-<HUDActionFlow />
-import HUDIAState from '../components/hud/HUDIAState';
-<HUDIAState />
-import HUDProfilePanel from '../components/hud/HUDProfilePanel';
-<HUDProfilePanel />
-import HUDMatrixGrid from '../components/hud/HUDMatrixGrid';
-<HUDMatrixGrid />
-import HUDThermalMap from '../components/hud/HUDThermalMap';
-<HUDThermalMap />
-import HUDOverdrivePulse from '../components/hud/HUDOverdrivePulse';
-<HUDOverdrivePulse />
-import HUDNeuralScan from '../components/hud/HUDNeuralScan';
-<HUDNeuralScan />
-import HUDSpectrumWave from '../components/hud/HUDSpectrumWave';
-<HUDSpectrumWave />
-import HUDQuantumField from '../components/hud/HUDQuantumField';
-<HUDQuantumField />
-import HUDCryoFlux from '../components/hud/HUDCryoFlux';
-<HUDCryoFlux />
-import HUDCryoFlux from '../components/hud/HUDCryoFlux';
-<HUDCryoFlux />
-import HUDVortexField from '../components/hud/HUDVortexField';
-<HUDVortexField />
